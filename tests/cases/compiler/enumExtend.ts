448bb42b1b9ba54eec91be306bc206dc0bec58c1
Jeff Mercado <JeffreyMercado@users.noreply.github.com>
2020-06-10 22:17:24 -0700

    creating basic test case

---
+++ tests/cases/compiler/enumExtend.ts
@@ -0,0 +1,18 @@
+enum A {
+    a, b, c
+}
+
+enum B {
+    ...A,
+    d, e, f
+}
+
+const Aa = A.a;
+const Ab = A.b;
+const Ac = A.c;
+const Ba = B.a;
+const Bb = B.b;
+const Bc = B.c;
+const Bd = B.d;
+const Be = B.e;
+const Bf = B.f;
